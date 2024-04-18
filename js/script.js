3c1d3174d262e69322ba37d4e57c688e3d8bca0b
Kredwi <kredwi888@gmail.com>
2024-04-19 02:50:37 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+(async () => {
+	const discord = await fetchData("https://discord.com/api/guilds/766538286214283265/widget.json");
+	restoreInfo("discordMembers", discord?.members?.length);
+	restoreInfo("discordChannels", discord?.channels?.length);
+	const github = await fetchData("https://api.github.com/users/kredwi");
+	restoreInfo("githubRepos", github?.public_repos);
+	restoreInfo("githubFollowing", github?.followers);
+	restoreInfo("githubFollowing", github?.following);
+})();
+const adaptiveClick = document.getElementById("enb");
+adaptiveClick.addEventListener("click", () => {
+ 	if (adaptiveClick.textContent.trim().toLowerCase() == "kredwi - тупость в коде") {
+  		adaptiveClick.innerHTML = "Kredwi - Доступность в коде"
+    	enableAdaptive(false);
+ 	} else {
+  		adaptiveClick.innerHTML = "Kredwi - Тупость в коде"
+		enableAdaptive(true);
+ 	}
+});
+function restoreInfo(id, number) {
+	document.getElementById(id).innerHTML = number;
+}
+async function fetchData(u, b) {
+	try {
+		const response = await fetch(u);
+		return await response.json();
+	} catch (error) {
+		console.error(error)
+	}
+}
+function enableAdaptive(e) {
+	if (!e) replaceCSS("css/style-adaptive.css");
+		else replaceCSS("css/style.css");
+}
+function replaceCSS(u) {
+	const newCSS = document.createElement("link");
+	newCSS.rel = 'stylesheet';
+	newCSS.href = u;
+	newCSS.id = "style";
+	const currentCSS = document.getElementById("style");
+	document.head.appendChild(newCSS);
+	currentCSS.parentNode.removeChild(currentCSS);
+}
