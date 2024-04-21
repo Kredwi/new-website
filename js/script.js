9e760c5fb7561589ee7d79877e32beeb61351039
Kredwi <74652542+Kredwi@users.noreply.github.com>
2024-04-21 15:57:46 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
-"use strict";
-(async () => {
-	const discord = await fetchData("https://discord.com/api/guilds/766538286214283265/widget.json");
-	restoreInfo("discordMembers", discord?.members?.length);
-	restoreInfo("discordChannels", discord?.channels?.length);
-	const githubLS = checkLocalStorage("github");
-	const github = await fetchData("https://api.github.com/users/kredwi");
-	if (github?.public_repos == undefined) {
-		restoreInfo("githubRepos", githubLS?.public_repos || 0);
-		restoreInfo("githubFollowing", githubLS?.followers || 0);
-		restoreInfo("githubFollowing", githubLS?.following || 0);
-		console.log("GitHub loaded old information from localStorage. Created (UNIX): " + (githubLS?.created || "Not Found"))
+"use strict"; // Строгий режим
+(async () => { // Анонимная функция
+	const discord = await fetchData("https://discord.com/api/guilds/766538286214283265/widget.json"); // Запрос данных с Discord
+	restoreInfo("discordMembers", discord?.members?.length); // Прорисовка количество участников в HTML дереве
+	restoreInfo("discordChannels", discord?.channels?.length); // Прорисовка количество каналов в HTML дереве
+	const githubLS = checkLocalStorage("github"); // Получение данных с LocalStorage
+	const github = await fetchData("https://api.github.com/users/kredwi"); // Запрос данных с GitHub
+	if (github?.public_repos == undefined) { // Проверка существует ли ключ "public_repos" в запросе с GitHub
+		restoreInfo("githubRepos", githubLS?.public_repos || 0); // Прорисовка количество репозиторий в HTML дереве
+		restoreInfo("githubFollowing", githubLS?.followers || 0); // Прорисовка количество фолловеров в HTML дереве
+		restoreInfo("githubFollowing", githubLS?.following || 0); // Прорисовка количество подписок в HTML дереве
+		console.log("GitHub loaded old information from localStorage. Created (UNIX): " + (githubLS?.created || "Not Found"));
 	} else {
-		restoreInfo("githubRepos", github?.public_repos || 0);
-		restoreInfo("githubFollowing", github?.followers || 0);
-		restoreInfo("githubFollowing", github?.following || 0);
-		localStorage.setItem("github", JSON.stringify({
+		restoreInfo("githubRepos", github?.public_repos || 0); // Прорисовка количество репозиторий в HTML дереве
+		restoreInfo("githubFollowing", github?.followers || 0); // Прорисовка количество фолловеров в HTML дереве
+		restoreInfo("githubFollowing", github?.following || 0); // Прорисовка количество подписок в HTML дереве
+		localStorage.setItem("github", JSON.stringify({ // Сохранение ифнормации в LocalStorage в JSON формате
 			public_repos: github?.public_repos || 0, 
 			followers: github?.followers || 0, 
 			following: github?.following || 0, 
-			created: new Date().getTime()
+			created: new Date().getTime() // Получаем UNIX время
 		}));
 	}
-	document.getElementById("enb").addEventListener("click", (event) => {
-	    const redirectToOldWeb = confirm("Открыть новую вкладку?");
-	    redirectToOldWeb ? window.open("https://kredwi.ru", "_blank") : window.open("https://kredwi.ru", "_self");
+	document.getElementById("enb").addEventListener("click", (event) => { // Добавляем обработчик нажатий на Footer-Текст
+	    const redirectToOldWeb = confirm("Открыть новую вкладку?"); // Спрашиваем, как открыть ссылку
+	    redirectToOldWeb ? window.open("https://kredwi.ru", "_blank") : window.open("https://kredwi.ru", "_self"); // Открываем ссылку
 	})
 })();
 function restoreInfo(id, number) {
-	document.getElementById(id).innerHTML = number;
+	document.getElementById(id).innerHTML = number; // Меняем значение в HTML дереве
 }
 async function fetchData(u, b) {
 	try {
 		const response = await fetch(u);
 		return await response.json();
 	} catch (error) {
-		console.error(error)
+		console.error(error);
 	}
 }
 function redirect(e) {
@@ -58,13 +58,9 @@ function redirect(e) {
 			throw new Error("Error rederection");
 	}
 }
-function truncateText(text, length) {
-	if (text.length > length) return text.substring(0, length).trim() + "...";
-	else return text;
-}
 function checkLocalStorage(n) {
-	return JSON.parse(localStorage.getItem(n));
+	return JSON.parse(localStorage.getItem(n)); // Возращаем данные с LocalStorage, принудительно преобразовываем в JSON
 }
 function toHours(h) {
-	return h * 60 * 60 * 1000;
+	return h * 60 * 60 * 1000; // Возращаем часы в UNIX-времени
 }
