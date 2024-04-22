443b150b6ef3950e9982a01850a65a2b673b233f
Kredwi <74652542+Kredwi@users.noreply.github.com>
2024-04-22 16:11:54 +0900

    Caching and new comments

--- js/script.js
+++ js/script.js
@@ -4,13 +4,8 @@
 	restoreInfo("discordMembers", discord?.members?.length); // Прорисовка количество участников в HTML дереве
 	restoreInfo("discordChannels", discord?.channels?.length); // Прорисовка количество каналов в HTML дереве
 	const githubLS = checkLocalStorage("github"); // Получение данных с LocalStorage
-	const github = await fetchData("https://api.github.com/users/kredwi"); // Запрос данных с GitHub
-	if (github?.public_repos == undefined) { // Проверка существует ли ключ "public_repos" в запросе с GitHub
-		restoreInfo("githubRepos", githubLS?.public_repos || 0); // Прорисовка количество репозиторий в HTML дереве
-		restoreInfo("githubFollowing", githubLS?.followers || 0); // Прорисовка количество фолловеров в HTML дереве
-		restoreInfo("githubFollowing", githubLS?.following || 0); // Прорисовка количество подписок в HTML дереве
-		console.log("GitHub loaded old information from localStorage. Created (UNIX): " + (githubLS?.created || "Not Found"));
-	} else {
+	if (!githubLS || (githubLS?.created - new Date().getTime() / 1000) >= toHours(3)) {// Проверяем, если в LocalStorage нет данных или время с момента их создания больше трех часов, то выполняем запрос к GitHub API для получения свежих данных
+		const github = await fetchData("https://api.github.com/users/kredwi"); // Запрос данных с GitHub
 		restoreInfo("githubRepos", github?.public_repos || 0); // Прорисовка количество репозиторий в HTML дереве
 		restoreInfo("githubFollowing", github?.followers || 0); // Прорисовка количество фолловеров в HTML дереве
 		restoreInfo("githubFollowing", github?.following || 0); // Прорисовка количество подписок в HTML дереве
@@ -20,15 +15,22 @@
 			following: github?.following || 0, 
 			created: Math.floor(new Date().getTime() / 1000) // Получаем UNIX время (секунды)
 		}));
+	} else {
+		restoreInfo("githubRepos", githubLS?.public_repos || 0); // Прорисовка количество репозиторий в HTML дереве
+		restoreInfo("githubFollowing", githubLS?.followers || 0); // Прорисовка количество фолловеров в HTML дереве
+		restoreInfo("githubFollowing", githubLS?.following || 0); // Прорисовка количество подписок в HTML дереве
+		console.log("GitHub loaded old information from localStorage. Created (UNIX): " + (githubLS?.created || "Not Found"));
 	}
 	document.getElementById("enb").addEventListener("click", (event) => { // Добавляем обработчик нажатий на Footer-Текст
 	    const redirectToOldWeb = confirm("Открыть новую вкладку?"); // Спрашиваем, как открыть ссылку
 	    redirectToOldWeb ? window.open("https://kredwi.ru", "_blank") : window.open("https://kredwi.ru", "_self"); // Открываем ссылку
 	})
 })();
+// Функция для изменения значения в HTML-дереве
 function restoreInfo(id, number) {
 	document.getElementById(id).innerHTML = number; // Меняем значение в HTML дереве
 }
+// Функция для получения данных с сервера
 async function fetchData(u, b) {
 	try {
 		const response = await fetch(u);
@@ -37,6 +39,7 @@ async function fetchData(u, b) {
 		console.error(error);
 	}
 }
+// Функция для открытия ссылки на видео/YouTube канал при клике на кнопку
 function redirect(e) {
 	const url = {
 		channel: "https://www.youtube.com/channel/UCH-f_szwD2msRXhwMmePeiA",
@@ -46,21 +49,23 @@ function redirect(e) {
 	}
 	switch (e) {
 		case 1:
-			window.open(url.channel, "_blank");
+			window.open(url.video1, "_blank");
 			break;
 		case 2:
-			window.open(url.channel, "_blank");
+			window.open(url.video2, "_blank");
 			break;
 		case 3:
-			window.open(url.channel, "_blank");
+			window.open(url.video3, "_blank");
 			break;
 		default:
 			throw new Error("Error rederection");
 	}
 }
+// Функция для проверки LocalStorage (Локального хранилище)
 function checkLocalStorage(n) {
 	return JSON.parse(localStorage.getItem(n)); // Возращаем данные с LocalStorage, принудительно преобразовываем в JSON
 }
+// Функция для преобразования часов в секунды
 function toHours(h) {
-	return h * 60 * 60 * 1000; // Возращаем часы в UNIX-времени
+	return h * 60 * 60; // Возращаем часы в UNIX-времени
 }
